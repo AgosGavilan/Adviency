90f61a5e569687b0ad07286f3a6b7932b91c0d02
Agostina Gavilan <agosgavilan3@gmail.com>
2022-05-31 01:44:07 -0300

    dia 8

--- src/components/addGift.tsx
+++ src/components/addGift.tsx
@@ -1,33 +1,63 @@
 import React, { useState } from "react";
 import s from '../style/add.module.css'
 
+
+//SIEMPRE QUE PASE UN ARRAY DE OBJETOS POR PARAMETRO, PRIMERO TENGO QUE CREAR UNA INTERFACE PARA EL OBJETO
+interface list {
+    nombre: string;
+    cantidad: number
+}
+
 type Props = { //mediante la palabra clave type permite crear nuevos tipos y luego reutilizarlos
     setLista: ([]) => void;
-    lista: string[];
+    lista: list[]; //Y DE ESTA MANERA SE TYPEA UN ARRAY DE OBJETOS
 }
 
 //export const AddGift: React.FC<Props> = ({setLista, lista}) => {} //Las FC pueden ser escritos como funciones normales que toman props como argumentos y retornan un elemento JSX
 export const AddGift = ({setLista, lista}: Props) => { 
-    const [regalo, setRegalo] = useState("")
+    const [regalo, setRegalo] = useState({
+        nombre: '',
+        cantidad: 1
+    })
 
     function handleChange(e: any) :void {
         e.preventDefault()
-        setRegalo(e.target.value)
+        setRegalo(prev => ({ //seteo mi estado local con el valor de mis inputs
+            ...prev, //hago una copia de lo que ya tenia en mi estado
+            [e.target.name]: e.target.value //y le agrego el valor de mis inputs
+        }))
     }
 
     function handleSubmit (e: any) :void{
         e.preventDefault()
-        setLista([...lista, regalo])
-        setRegalo("")
+        let noRepeat = lista.filter(el => el.nombre === regalo.nombre)
+        if(noRepeat.length !== 0) {
+            alert("Ya existe un regalo con ese nombre")
+        }
+        else {setLista([...lista, regalo])}
+        setRegalo({
+            nombre: '',
+            cantidad: 1
+        })
     }
     return (
         <div>
             <form onSubmit={handleSubmit} className={s.form}>
                 <input
                 className={s.input}
+                required
                 type='text'
-                value={regalo}
+                name="nombre"
+                value={regalo.nombre}
                 placeholder='Añadir regalo'
+                onChange={handleChange}
+                />
+                <input
+                className={s.cantidad}
+                type='number'
+                min={1}
+                value={regalo.cantidad}
+                name='cantidad'
                 onChange={handleChange}
                 />
                 <button
--- src/components/inicio.tsx
+++ src/components/inicio.tsx
@@ -6,15 +6,29 @@ import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 
 export const Inicio = () : JSX.Element => {
-    const [lista, setLista] = useState(["medias", "zapatillas", "carteras"])
+    //const [lista, setLista] = useState(["medias", "zapatillas", "carteras"])
+    const [lista, setLista] = useState([
+        {
+            nombre: "medias",
+        cantidad: 1
+        },
+        {
+            nombre: "zapatillas",
+        cantidad: 1
+        },
+        {
+            nombre: "carteras",
+        cantidad: 1
+        }
+    ])
 
-    function handleDelete(r: string) :void { //por parametro me llego el regalo que tengo que eliminar
-        setLista(lista.filter(g => g !== r)) //y le digo que filtre por aquellos regalos que no se llamen igual que mi regalo a eliminar
+    function handleDelete(r: any) :void { //por parametro me llego el regalo que tengo que eliminar
+        setLista(lista.filter(g => g.nombre !== r)) //y le digo que filtre por aquellos regalos que no se llamen igual que mi regalo a eliminar
     }
 
-    function deleteAll (r: any) {
+    function deleteAll (r: any){
         return lista.length > 0 
-        ? setLista(lista.filter(g => g === r))
+        ? setLista(lista.filter(g => g.nombre === r))
         : ''
     }
 
@@ -27,12 +41,12 @@ export const Inicio = () : JSX.Element => {
                 </div>
                 <AddGift setLista={setLista} lista={lista}/>
                 {lista.length ? lista.map(r => (
-                        <ul key={r}>
-                            <li key={r} className={s.li}>
-                                {r}
+                        <ul key={r.nombre}>
+                            <li key={r.nombre} className={s.li}>
+                                {`${r.nombre} x${r.cantidad == null ? 1 : r.cantidad}` }
                                 <span>
                                     <button //al boton de eliminar le tengo que decir QUE ELEMENTO de mi array de regalos tienen que eliminar
-                                    onClick={()=> handleDelete(r)} //asi que por parametro le paso a la funcion el elemento (r)
+                                    onClick={()=> handleDelete(r.nombre)} //asi que por parametro le paso a la funcion el elemento (r)
                                     className={s.trash}
                                     >
                                         <FontAwesomeIcon icon={faTrashCan} />
